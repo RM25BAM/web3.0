ccc6bea838bf3c2ba0612d93b23b1b78c4477479
RM25BAM <nmpiedrabuena2003@gmail.com>
2023-10-12 00:05:03 -0400

    setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icon/ai';
+
+import logo from '../../images/logo.png';
+
+const Navbar = () => {
+    return (
+        <h1>
+            Navbar
+        </h1>
+
+
+
+    );
+}
+export default Navbar;
