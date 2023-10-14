1f1cff5ca41d672fe1909d6ad70e7c913edcf42b
RM25BAM <nmpiedrabuena2003@gmail.com>
2023-10-14 13:10:49 -0400

    Some changes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,12 +1,22 @@
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
+
+import { Loader } from './';
+
 const Welcome = () => {
     return (
-        <h1>
-            Welcome
-        
-        </h1>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1 ">
+                        Send Crypto <br /> across the World
+                    </h1>{/*  ---- change the font to an animated one morphism*/}
+                    
 
-
-
+                </div>
+            </div>
+        </div>
     );
 }
 export default Welcome;
